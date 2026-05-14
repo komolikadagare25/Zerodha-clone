a50e69674ba0c29fdb2bff6a21cc402771d6fedb
komolika07 <dagarekomolika2004@gmail.com>
2026-05-14 11:37:31 +0530

    bootStrap and fontawesome included

--- frontend/src/Landing_page/home/HeroSection.js
+++ frontend/src/Landing_page/home/HeroSection.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function HeroSection() {
     return (  
-        <h1>Hero Section</h1>
+        <h1>Hero Section <i class="fa fa-user-circle" aria-hidden="true"></i></h1>
     );
 }
 

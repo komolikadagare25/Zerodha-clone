8f5db96f328d606477e118fbb07f009e752f8ded
komolika07 <dagarekomolika2004@gmail.com>
2026-06-10 16:33:06 +0530

    navbar and footer implemented

--- frontend/src/Landing_page/TopNav.js
+++ frontend/src/Landing_page/TopNav.js
@@ -1,9 +1,42 @@
 import React from 'react'
 
 function TopNav() {
-    return ( 
-        <h1>TopNav</h1>
-     );
+    return (
+
+        <nav class="navbar navbar-expand-lg border-bottom p-3" style={{ backgroundColor: '#ffff' }}>
+            <div class="container">
+                <a class="navbar-brand" href="#">
+                    <img src="media/images/logo.svg" alt="Logo" style={{ width: '25%', height: '4%' }} />
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+                    <form class="d-flex" role="search">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                             <li class="nav-item">
+                                <a class="nav-link active" href="#">Signup</a>
+                            </li>
+                             <li class="nav-item">
+                                <a class="nav-link active" href="#">About</a>
+                            </li>
+                             <li class="nav-item">
+                                <a class="nav-link active" href="#">Products</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="#">Pricing</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="#">Support</a>
+                            </li>  
+                        </ul>
+                    </form>
+                </div>
+            </div>
+        </nav>
+
+    );
 }
 
 export default TopNav;

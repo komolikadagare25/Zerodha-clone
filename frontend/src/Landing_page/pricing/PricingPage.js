0fa7da8693a71edc00a41bf0a7bac81c4e92c8c5
komolika07 <dagarekomolika2004@gmail.com>
2026-06-11 20:27:12 +0530

    Pricing Page done

--- frontend/src/Landing_page/pricing/PricingPage.js
+++ frontend/src/Landing_page/pricing/PricingPage.js
@@ -3,12 +3,14 @@ import TopNav from '../TopNav';
 import Footer from '../Footer';
 import Hero from './Hero';
 import Brokerage from './Brokerage';
+import OpenAccount from "../OpenAccount";
 
 function PricingPage() {
     return ( 
         <>
         
         <Hero></Hero>
+        <OpenAccount></OpenAccount>
         <Brokerage></Brokerage>
         
         </>
